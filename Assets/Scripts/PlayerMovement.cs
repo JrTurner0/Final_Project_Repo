7afacd36ba0986a56c2522cb3cd32e1b94d33185
JrTurner0 <jrturner1@unoh.edu>
2025-11-13 13:02:28 -0500

    FinalV2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 4.0f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+
+        }
+    }
+}
